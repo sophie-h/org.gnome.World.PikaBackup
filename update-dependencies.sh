1a7c30c863dca5591b218afdbb40842792b52e97
Sophie Herold <sophie@hemio.de>
2026-08-04 22:01:22 +0200

    0.8.4

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -11,6 +11,8 @@ fi
 commit="$(git -C "${PROJECT_DIR}" rev-parse HEAD)"
 yq ".modules[-1].sources[0].commit = \"${commit}\"" "${PROJECT_DIR}/build-aux/${APP_ID}.yml" > "${APP_ID}.yml"
 
+flatpak run org.flathub.flatpak-external-data-checker --edit-only org.gnome.World.PikaBackup.yml
+
 ./flatpak-builder-tools/cargo/flatpak-cargo-generator.py "${PROJECT_DIR}/Cargo.lock"
 
 ./flatpak-builder-tools/pip/flatpak-pip-generator tomli
